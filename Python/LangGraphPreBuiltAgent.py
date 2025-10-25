6c92f00febf871bf409d97dc64de7dd151d9a6f9
Prakash <prakash.manit@gmail.com>
2025-10-25 19:10:22 +0530

    Added Tools in agent

--- Python/LangGraphPreBuiltAgent.py
+++ Python/LangGraphPreBuiltAgent.py
@@ -1,25 +1,44 @@
 def LangGrPreBAgentFn():
+    import os
     from dotenv import load_dotenv
     load_dotenv()
-
     from langgraph.prebuilt import create_react_agent
+
+    def addFile(filename: str) -> None:
+        """Create a new file in current directory"""
+        if not os.path.exists(filename):
+            with open(filename, "w") as f:
+                pass
+            print(f"File '{filename}' created.")
+        else:
+            print(f"File '{filename}' already exists.")
+        
+    def addFolder(directory_name: str):
+        """Create a new Directory in current directory"""
+        if not os.path.exists(directory_name):
+            os.mkdir(directory_name)
+            print(f"Directory '{directory_name}' created.")
+        else:
+            print(f"Directory '{directory_name}' already exists.")
 
     agent = create_react_agent(
         model="groq:llama-3.3-70b-versatile", 
-        tools=[], 
+        tools=[addFile, addFolder], 
         prompt="You are a helpful assistant" 
     )
 
-    # Save graph in image
-    try:
-        img = agent.get_graph().draw_mermaid_png()
-        with open("graph2.png", "wb") as f:
-            f.write(img)
-    except Exception:
-        pass
+    # # Save graph in image
+    # try:
+    #     img = agent.get_graph().draw_mermaid_png()
+    #     with open("graph2.png", "wb") as f:
+    #         f.write(img)
+    # except Exception:
+    #     pass
 
     # Run the agents
+    # prompt= "create a new file with name test.txt in the current location"
+    prompt= "create a new directory with name Test in the current location and inside that create a file named example.txt"
     response = agent.invoke(
-        {"messages": [{"role": "user", "content": "what are large language models"}]}
-    )
+        {"messages": [{"role": "user", "content": prompt}]}
+    )   
     print(response)
